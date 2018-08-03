de15cafe44fb657d69ce804a775c0a48f0317150
Usman Azam <UsmanA@GB-RD1LPW100130.mastek.com>
2018-08-03 14:07:57 +0100

    Further jquery learning examples with some interactive buttons

--- trunk/mustache/src/app/app.component.spec.ts
+++ trunk/mustache/src/app/app.component.spec.ts
@@ -24,4 +24,10 @@ describe('AppComponent', () => {
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('h1').textContent).toContain('Welcome to mustache!');
   }));
+  it('should render title in a h1 tag', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to mustache!');
+  }));
 });
---
+++ trunk/mustache/src/app/extras/jQueryLearning.component.js
@@ -0,0 +1,77 @@
+function hide_basic_template() {
+    $('#person').hide();
+    $('#BasicTemplate').hide();
+}
+
+function show_basic_template() {
+    $('#person').show();
+    $('#BasicTemplate').show();
+}
+
+function hide_Enumerable_section() {
+    $('#EnumerableSection').hide();
+}
+
+function show_Enumerable_section() {
+    $('#EnumerableSection').show();
+}
+
+function hide_Enumerable_section_with_objects() {
+    $('#EnumerableSectionWithObjects').hide();
+}
+
+function show_Enumerable_section_with_objects() {
+    $('#EnumerableSectionWithObjects').show();
+}
+
+function hide_nested_objects() {
+    $('#nestedObjects').hide();
+}
+
+function show_nested_objects() {
+    $('#nestedObjects').show();
+}
+
+function hide_dereferencing_example() {
+    $('#dereferencing').hide();
+}
+function show_dereferencing_example() {
+    $('#dereferencing').show();
+}
+
+function show_function_reference_example() {
+    $('#functionReference').show();
+}
+
+function hide_function_reference_example() {
+    $('#functionReference').hide();
+}
+
+function show_conditional_section_example() {
+    $('#conditionSections').show();
+}
+
+function hide_conditional_section_example() {
+    $('#conditionSections').hide();
+}
+
+function show_Partials_example() {
+    $('#partials').show();
+}
+
+function hide_Partials_example() {
+    $('#partials').hide();
+}
+
+function show_Partials_in_enumerable_section_example() {
+    $('#partialsEnumerableSection').show();
+}
+
+function hide_Partials_in_enumerable_section_example() {
+    $('#partialsEnumerableSection').hide();
+}
+
+function hide_show_all_p_tags() {
+    $("p").toggle();
+}
+
